2d7c18fa75e1c7e42d940dfbf849d252af9f14ae
hamza887 <hamzabutt887>
2021-06-28 04:04:19 -0400

    completed the basic functionality on my search engine

---
+++ script.js
@@ -0,0 +1,22 @@
+
+const pokeappear=document.getElementById('pokeappear');
+const pokepic=document.getElementById('pokepic');
+let inputval=0;
+ let API_POKEMON= 'https://pokeapi.co/api/v2/pokemon/';
+
+const getUser=async()=>{
+  const SEARCH_STR=API_POKEMON+inputval;
+  const response= await fetch(SEARCH_STR);
+  const data= await response.json();
+  pokeappear.innerHTML=`<h2>${data.name}</h2>`
+  pokepic.innerHTML=`<img src=${data.sprites.front_default}></img>`
+}
+
+
+function ShowPokemon(form){
+   inputval= form.inputbox.value
+  console.log(inputval);
+  getUser();
+}
+
+
